c09d8016ef5745d2e65958f83b87e2a26fdb46df
Aaron Burmeister <aaron.b98@web.de>
2016-12-26 12:15:09 +0100

    261216-01

--- app/src/main/java/de/ab/reflect/MainActivity.java
+++ app/src/main/java/de/ab/reflect/MainActivity.java
@@ -1,10 +1,17 @@
 package de.ab.reflect;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.CheckBox;
@@ -13,8 +20,11 @@ import android.widget.EditText;
 
 import java.io.File;
 
+import de.ab.dialog.FileChooser;
+import de.ab.dialog.SimpleFileChooser;
 import de.ab.reflect.ssh.SSHConfiguration;
 
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 import static de.ab.reflect.ssh.SSHConfiguration.HOST;
 import static de.ab.reflect.ssh.SSHConfiguration.KNOWN_HOSTS_FILE;
 import static de.ab.reflect.ssh.SSHConfiguration.PASSPHRASE;
@@ -38,10 +48,59 @@ public class MainActivity extends Activity {
 			CONFIG_PASSPHRASE = "passphrase",
 			CONFIG_SAVE_CONFIG = "saveConfig",
 			CONFIG_SAVE_PASSWORDS = "savePasswords";
+	private static final int REQUEST_PERMISSION = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+			initPermissions();
+		else
+			init();
+	}
+
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+	private void initPermissions() {
+		if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PERMISSION_GRANTED)
+			init();
+		else if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+			AlertDialog dialog = new AlertDialog.Builder(this)
+					.setTitle(R.string.storage_permission)
+					.setMessage(R.string.storage_permission_message)
+					.setCancelable(true)
+					.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialogInterface, int i) {
+							dialogInterface.cancel();
+						}
+					}).create();
+			dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+				@Override
+				public void onCancel(DialogInterface dialogInterface) {
+					ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
+				}
+			});
+			dialog.show();
+		} else
+			ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		if (requestCode == REQUEST_PERMISSION) {
+			boolean grantedAll = true;
+			for (int i : grantResults)
+				if (i != PERMISSION_GRANTED)
+					grantedAll = false;
+			if (!grantedAll)
+				initPermissions();
+			else
+				init();
+		}
+	}
+
+	private void init() {
 		setContentView(R.layout.main);
 
 		SharedPreferences pref = getConfig();
@@ -60,8 +119,11 @@ public class MainActivity extends Activity {
 
 		setCompoundBinding(R.id.password_enabled, R.id.password);
 		setCompoundBinding(R.id.passphrase_enabled, R.id.passphrase);
-		setCompoundBinding(R.id.auth_enabled, R.id.auth, R.id.passphrase_enabled, R.id.passphrase, R.id.known_hosts);
+		setCompoundBinding(R.id.auth_enabled, R.id.auth, R.id.auth_select, R.id.passphrase_enabled, R.id.passphrase, R.id.known_hosts, R.id.known_hosts_select);
 		setCompoundBinding(R.id.save_config, R.id.save_passwords);
+
+		setFileChooserBinding(R.id.auth, R.id.auth_select);
+		setFileChooserBinding(R.id.known_hosts, R.id.known_hosts_select);
 	}
 
 	private void setCompoundBinding(@IdRes int idCompound, @IdRes int... idBindings) {
@@ -94,13 +156,30 @@ public class MainActivity extends Activity {
 		execInstance.run(compoundButton.isChecked());
 	}
 
+	private void setFileChooserBinding(@IdRes final int editText, @IdRes int button) {
+		findViewById(button).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				SimpleFileChooser chooser = new SimpleFileChooser(MainActivity.this);
+				chooser.setMode(FileChooser.MODE_FILES);
+				chooser.setOnFileSelectedListener(new FileChooser.OnFileSelectedListener() {
+					@Override
+					public void onFileSelected(FileChooser chooser, String resultPath) {
+						((EditText) findViewById(editText)).setText(resultPath);
+					}
+				});
+				chooser.show();
+			}
+		});
+	}
+
 	public void connect(View v) {
-		String user = ((EditText) findViewById(R.id.user)).getText().toString().trim();
-		boolean passwordEnabled = ((CheckBox) findViewById(R.id.password_enabled)).isChecked();
-		String password = ((EditText) findViewById(R.id.password)).getText().toString();
 		String host = ((EditText) findViewById(R.id.host)).getText().toString().trim();
 		String port = ((EditText) findViewById(R.id.port)).getText().toString().trim();
 		int portNum;
+		String user = ((EditText) findViewById(R.id.user)).getText().toString().trim();
+		boolean passwordEnabled = ((CheckBox) findViewById(R.id.password_enabled)).isChecked();
+		String password = ((EditText) findViewById(R.id.password)).getText().toString();
 		boolean authEnabled = ((CheckBox) findViewById(R.id.auth_enabled)).isChecked();
 		String auth = ((EditText) findViewById(R.id.auth)).getText().toString().trim();
 		boolean passphraseEnabled = ((CheckBox) findViewById(R.id.passphrase_enabled)).isChecked();
@@ -110,10 +189,6 @@ public class MainActivity extends Activity {
 		boolean saveConfig = ((CheckBox) findViewById(R.id.save_config)).isChecked();
 		boolean savePasswords = ((CheckBox) findViewById(R.id.save_passwords)).isChecked();
 
-		if (user.trim().length() == 0) {
-			format(R.id.user);
-			return;
-		}
 		if (host.trim().length() == 0) {
 			format(R.id.host);
 			return;
@@ -125,6 +200,10 @@ public class MainActivity extends Activity {
 			}
 		} catch (NumberFormatException e) {
 			format(R.id.port);
+			return;
+		}
+		if (user.trim().length() == 0) {
+			format(R.id.user);
 			return;
 		}
 		if (authEnabled && (auth.trim().length() == 0 || !new File(auth).isFile())) {
